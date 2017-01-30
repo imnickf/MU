358fb009ad6627f0830dd1263f818c07f295e7ff
Nick Flege <nflege13@gmail.com>
2017-01-29 20:31:19 -0600

    Update User to encrypt password

--- Experiments/app/models/user.js
+++ Experiments/app/models/user.js
@@ -1,11 +1,45 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var bcrypt = require('bcrypt');
 
 var UserSchema = new Schema({
-    name: String,
-    password: String,
+    name: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
     admin: Boolean,
     moderator: Boolean
 });
+
+UserSchema.pre('save', function(next) {
+    var user = this;
+    if (this.isModified('password') || this.isNew) {
+        bcrypt.genSalt(10, function(err, salt) {
+            if (err)
+                next(err);
+            bcrypt.hash(user.password, salt, function(err, hash) {
+               if (err)
+                   next(err);
+               user.password = hash;
+               next();
+            });
+        });
+    } else {
+        return next();
+    }
+});
+
+UserSchema.methods.compare = function (passw, cb) {
+    bcrypt.compare(passw, this.password, function(err, isMatch) {
+        if (err)
+            return cb(err);
+        cb(null, isMatch);
+    });
+};
 
 module.exports = mongoose.model('User', UserSchema);
