4422d1e84cd0f8e60b498bd676cb5bd8bae1b0e3
Nick Flege <nflege13@gmail.com>
2017-01-29 20:32:53 -0600

    Remove endpoint for creating users

--- Experiments/api/users.js
+++ Experiments/api/users.js
@@ -5,21 +5,6 @@ var User = require('../app/models/user');
 
 router.route('/users')
 
-// create a user (accessed at POST http://localhost:8080/api/users)
-    .post(function(req, res) {
-
-        var user = new User();      // create a new instance of the User model
-        user.name = req.body.name;  // set the users name (comes from the request)
-
-        // save the user and check for errors
-        user.save(function(err) {
-            if (err)
-                res.send(err);
-
-            res.json({ message: 'User created!' });
-        });
-    })
-
     // GET all users (accessed at GET http://localhost:8080/api/users)
     .get(function(req, res) {
         User.find(function(err, users) {
