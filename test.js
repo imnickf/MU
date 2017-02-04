6e06ee76751df9c5db06553c0377e7475736750b
fergie7 <fergie7@iastate.edu>
2017-02-03 22:32:14 -0600

    Further Node.js Experiments

--- test.js
+++ test.js
@@ -1,29 +1,46 @@
-var http = require("http");
-var connect = require("connect");
-var fs = require("fs");
+var express = require("express");
+var path = require("path");
+var router =  express.Router();
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+var app = express();
 
-var app = connect();
+var totalval = 0;
+
+//app.set("views", __dirname + "bla");
+//app.set('view engine', 'ejs');
+
+router.get("/", function (req, res) {
+           res.sendFile(__dirname + "/bla/index.html");
+           res.render('./bla/index', {total: totalval});
+});
+
+app.use("/", router);
+app.post("/process", urlencodedParser, function(req, res) {
+            response = {
+                x: req.body.x,
+                y: req.body.y
+            };
+         
+        total = +response.x + +response.y;
+         console.log(response.x + " + " + response.y + " = " + total);
+         
+         res.app.route("/");
+});
+
+app.use(express.static("bla"));
+
+var server = app.listen(8800);
 
 
-app.use("/bla", printMsg);
-app.use("/text", printText);
 
-http.createServer(app).listen(8800);
-
-function printMsg(request, response) {
-    console.log("Printing Msg!");
-    response.writeHead(200, {'Content-Type': 'text/html'});
-    fs.createReadStream("./index.html").pipe(response);
-
-}
-
-function printText(request, response) {
+/*function printText(request, response) {
     console.log("Printing Text!");
     response.writeHead(200, {'Content-Type': 'text/plain'});
     response.write("CatDog");
     response.end();
 }
-/*function (request, response) {
+function (request, response) {
 
     //setTimeout(function() {
    //     console.log("Work Done!");
@@ -37,5 +54,7 @@ function printText(request, response) {
    response.write("Cat");
    //response.end();
 }).listen(8000);*/
+
+
 //Print to server console.
-console.log('Server running at http://127.0.0.1:8000/');
+console.log('Server running at http://regieferg.student.iastate.edu:8800/');
