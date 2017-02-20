012e9359f7d80424ec4bfe6bef160ffd2d1bead5
Nick Flege <nflege13@gmail.com>
2017-02-19 19:31:51 -0600

    Setup books tableview

---
+++ iOS/MU/MU/BookTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  BookTableViewCell.swift
+//  MU
+//
+//  Created by Nick Flege on 2/19/17.
+//  Copyright © 2017 Nick Flege. All rights reserved.
+//
+
+import UIKit
+
+class BookTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
