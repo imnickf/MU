7180931b484f506c7bd7156c1d822195f0a79eaf
jkuczek <jkuczek@iastate.edu>
2017-02-24 11:18:52 -0600

    final login updates

--- controllers/main/app.js
+++ controllers/main/app.js
@@ -15,7 +15,20 @@ firebase.initializeApp(config);
 // define our angular app, our dependencies are ngRoute, firebase, and ui.bootstrap
 var app = angular.module('app', ['ngRoute', 'firebase', 'ui.bootstrap']);
 
-// controller for handling the navagation bar
+// controller for handling the navagation bar and header data
 angular.module('app').controller('navController', ['$scope', '$firebaseObject', function($scope, $firebaseObject){
     $scope.isNavCollapsed = true;
+
+    // listen for authentication changes
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user){
+            // user is logged in
+            $scope.loggedInText = "You are currently logged in as " + user.email;
+        }else{
+            // user logged out
+            $scope.loggedInText = "You are not logged in.";
+        }// end if we have a valid user
+
+        $scope.$apply();
+    });
 }]);
