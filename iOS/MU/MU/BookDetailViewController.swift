e4348dcd783b0650927fb50fc3ff3c638edb6e30
nick flege <nflege@iastate.edu>
2017-03-21 19:01:00 -0500

    Implement editing a book

--- iOS/MU/MU/BookDetailViewController.swift
+++ iOS/MU/MU/BookDetailViewController.swift
@@ -17,7 +17,7 @@ class BookDetailViewController: UIViewController
   @IBOutlet weak var classCodeLabel: UILabel!
   @IBOutlet weak var descriptionTextView: UITextView!
 
-  @IBOutlet weak var buyButton: UIButton!
+  @IBOutlet weak var actionButton: UIButton!
   @IBOutlet weak var isbnInfoLabel: UILabel!
   @IBOutlet weak var classCodeInfoLabel: UILabel!
 
@@ -42,10 +42,38 @@ class BookDetailViewController: UIViewController
 
     classCodeLabel.isHidden = book.classCode == nil
     classCodeInfoLabel.isHidden = book.classCode == nil
+
+    if ItemRepository().getUserID() == book.creatorID {
+      actionButton.setTitle("Mark Sold", for: .normal)
+      navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .edit, target: self, action: #selector(editItem))
+    } else {
+      actionButton.setTitle("Message Seller", for: .normal)
+      navigationItem.rightBarButtonItem = nil
+    }
   }
 
-  @IBAction func buyItem(_ sender: Any)
+  @objc fileprivate func editItem()
   {
-    
+    performSegue(withIdentifier: "editBook", sender: self)
+  }
+
+  @IBAction func actionButtonPressed(_ sender: UIButton)
+  {
+
+  }
+}
+
+// MARK: - Navigation
+
+extension BookDetailViewController
+{
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?)
+  {
+    if segue.identifier == "editBook" {
+      if let vc = segue.destination as? CreateBookTableViewController {
+        vc.shouldEdit = true
+        vc.book = book
+      }
+    }
   }
 }
