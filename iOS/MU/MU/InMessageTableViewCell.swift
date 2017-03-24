c760df4dcde796ed1c0690f8b9f34e995f23a664
nick flege <nflege@iastate.edu>
2017-03-24 00:18:09 -0500

    Add classes for two types of message table view cells

---
+++ iOS/MU/MU/InMessageTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  InMessageTableViewCell.swift
+//  MU
+//
+//  Created by Nick Flege on 3/24/17.
+//  Copyright © 2017 Nick Flege. All rights reserved.
+//
+
+import UIKit
+
+class InMessageTableViewCell: UITableViewCell
+{
+  @IBOutlet weak var messageContainerView: UIView!
+  @IBOutlet weak var messageLabel: UILabel!
+  
+  override func awakeFromNib()
+  {
+    super.awakeFromNib()
+  }
+}
---
+++ iOS/MU/MU/OutMessageTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  OutMessageTableViewCell.swift
+//  MU
+//
+//  Created by Nick Flege on 3/24/17.
+//  Copyright © 2017 Nick Flege. All rights reserved.
+//
+
+import UIKit
+
+class OutMessageTableViewCell: UITableViewCell
+{
+  @IBOutlet weak var messageContainerView: UIView!
+  @IBOutlet weak var messageLabel: UILabel!
+
+  override func awakeFromNib()
+  {
+    super.awakeFromNib()
+  }
+}
