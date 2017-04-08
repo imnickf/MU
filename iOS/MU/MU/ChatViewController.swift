38d5a0596ed4022694eea08c18b8b8966c1d78b8
nick flege <nflege@iastate.edu>
2017-04-08 14:25:56 -0500

    Add ability to display chat messages

--- iOS/MU/MU/ChatViewController.swift
+++ iOS/MU/MU/ChatViewController.swift
@@ -13,10 +13,12 @@ class ChatViewController: UIViewController
   @IBOutlet weak var messagesTableView: UITableView!
   @IBOutlet weak var sendMessageButton: UIButton!
   @IBOutlet weak var messageTextField: UITextField!
+  @IBOutlet weak var messageInputView: UIView!
 
   let chatRepo = ChatRepository()
   var receiverID: String!
-  var chat: Chat!
+  var chat: Chat?
+  var messages = [Message]()
 
   override func viewDidLoad()
   {
@@ -24,24 +26,50 @@ class ChatViewController: UIViewController
 
     chatRepo.getChat(withUserID: receiverID) { (chat) in
       self.chat = chat
-      // TODO: Set source for table view
+      self.messages = chat.messages
+      
+      self.messagesTableView.reloadData()
     }
   }
 
   @IBAction func sendMessage()
   {
     if let message = messageTextField.text {
-      chat.send(message: message)
+      chat?.send(message: message)
+      messageTextField.text = nil
     }
   }
 }
 
-// MARK: - Navigation
+extension ChatViewController: UITableViewDelegate
+{
 
-extension ChatViewController {
+}
 
-  override func prepare(for segue: UIStoryboardSegue, sender: Any?)
+extension ChatViewController: UITableViewDataSource
+{
+  func numberOfSections(in tableView: UITableView) -> Int
   {
+    return 1
+  }
 
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+  {
+    return messages.count
+  }
+
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+  {
+    var cell: UITableViewCell
+    let message = messages[indexPath.row]
+    if message.senderID == receiverID {
+      cell = tableView.dequeueReusableCell(withIdentifier: "inMessageCell", for: indexPath)
+      (cell as? InMessageTableViewCell)?.messageLabel.text = message.content
+    } else {
+      cell = tableView.dequeueReusableCell(withIdentifier: "outMessageCell", for: indexPath)
+      (cell as? OutMessageTableViewCell)?.messageLabel.text = message.content
+    }
+
+    return cell
   }
 }
