8f58d8dfd003b3da68812478b7d408012c614bd0
nick flege <nflege@iastate.edu>
2017-04-08 12:27:04 -0500

    Update Chat and Message class to add ability to save to database

--- iOS/MU/MU/Chat.swift
+++ iOS/MU/MU/Chat.swift
@@ -14,18 +14,32 @@ class Chat
   var receiverID: String
   var senderID: String
   var id: String
+  var persisted: Bool
+  var chatRepo: ChatRepository
 
-  init(id: String, receiverID: String, senderID: String)
+  init(id: String, receiverID: String, senderID: String, persisted: Bool)
   {
     self.id = id
     self.receiverID = receiverID
     self.senderID = senderID
+    self.persisted = persisted
     self.messages = []
+    self.chatRepo = ChatRepository()
   }
 
   func store(messages: [Message])
   {
     self.messages = messages
+  }
+
+  func send(message: String)
+  {
+    if !persisted {
+      chatRepo.persistNew(chat: self)
+    }
+    let message = Message(senderID: self.senderID, content: message)
+    messages.append(message)
+    chatRepo.persistNew(message: message, forChatID: self.id)
   }
 }
 
@@ -33,10 +47,12 @@ class Message
 {
   var senderID: String
   var content: String
+  var date: Date
 
   init(senderID: String, content: String)
   {
     self.senderID = senderID
     self.content = content
+    self.date = Date()
   }
 }
