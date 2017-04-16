104a6e1d180d0f7e653a4919271157d5783a32db
nick flege <nflege@iastate.edu>
2017-04-15 23:47:41 -0500

    Add handling to display all active chats in chat table view

--- iOS/MU/MU/ChatListTableViewController.swift
+++ iOS/MU/MU/ChatListTableViewController.swift
@@ -10,9 +10,18 @@ import UIKit
 
 class ChatListTableViewController: UITableViewController
 {
+  let userRepo = UserRespository()
+  var chatUsers = [UserMetaData]()
+  var selectedChat: UserMetaData?
+
   override func viewDidLoad()
   {
     super.viewDidLoad()
+
+    userRepo.getUsersChatList { (userData) in
+      self.chatUsers = userData
+      self.tableView.reloadData()
+    }
   }
 }
 
@@ -20,7 +29,6 @@ class ChatListTableViewController: UITableViewController
 
 extension ChatListTableViewController
 {
-
   override func numberOfSections(in tableView: UITableView) -> Int
   {
     return 1
@@ -28,65 +36,40 @@ extension ChatListTableViewController
 
   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
   {
-    // #warning Incomplete implementation, return the number of rows
-    return 0
+    return chatUsers.count
   }
 
-  /*
-   override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-   let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+  {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "chatListCell", for: indexPath) as! ChatListTableViewCell
+    cell.displayNameLabel.text = chatUsers[indexPath.row].displayName
+    return cell
+  }
+}
 
-   // Configure the cell...
+// Mark: Table view delegate
 
-   return cell
-   }
-   */
-
-  /*
-   // Override to support conditional editing of the table view.
-   override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-   // Return false if you do not want the specified item to be editable.
-   return true
-   }
-   */
-
-  /*
-   // Override to support editing the table view.
-   override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-   if editingStyle == .delete {
-   // Delete the row from the data source
-   tableView.deleteRows(at: [indexPath], with: .fade)
-   } else if editingStyle == .insert {
-   // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-   }
-   }
-   */
-
-  /*
-   // Override to support rearranging the table view.
-   override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-   }
-   */
-
-  /*
-   // Override to support conditional rearranging of the table view.
-   override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-   // Return false if you do not want the item to be re-orderable.
-   return true
-   }
-   */
+extension ChatListTableViewController
+{
+  override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
+  {
+    selectedChat = chatUsers[indexPath.row]
+    performSegue(withIdentifier: "showChatWithUser", sender: self)
+  }
 }
 
 // Mark: - Navigation
 
 extension ChatListTableViewController
 {
-  /*
-   // In a storyboard-based application, you will often want to do a little preparation before navigation
-   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-   // Get the new view controller using segue.destinationViewController.
-   // Pass the selected object to the new view controller.
-   }
-   */
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?)
+  {
+    if segue.identifier == "showChatWithUser" {
+      if let vc = segue.destination as? ChatViewController {
+        if let chat = selectedChat {
+          vc.receiverID = chat.userID
+        }
+      }
+    }
+  }
 }
